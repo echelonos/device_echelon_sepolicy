5cff603ca12250d78d58bba58e56c0cdcdf5b7a2
EnesSastim <sastimenes@gmail.com>
2019-01-28 18:48:18 +0000

    Echelon Bringup

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from echelon
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/echelon/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/echelon/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/echelon/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from echelon
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/echelon/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/echelon/sepolicy/qcom/common \
+    device/echelon/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
